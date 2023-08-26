bd584ff0741cdb4b8d9bcf0ab2e3d14db9445bc3
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-25 22:27:50 -0400

    add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
-import { OrbitControls } from '@react-three/drei'
-import { Html } from '@react-three/drei'
-import Navbar from './Navbar.tsx'
-import Homepage from './Homepage.tsx'
+import { useState } from 'react';
+import { OrbitControls } from '@react-three/drei';
+import { Html } from '@react-three/drei';
+import Navbar from './Navbar.tsx';
+import Homepage from './Homepage.tsx';
+import Lenis from '@studio-freight/lenis';
+import ProgressBar from './ProgressBar.tsx';
 
+import Blob from './Blob.tsx';
 
-import Blob from'./Blob.tsx'
-
-
-import './App.css'
+import './App.css';
 
 function App() {
+  const lenis = new Lenis();
 
+  lenis.on('scroll', (e: any) => {
+    // console.log(e);
+  });
 
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
-    <div className='h-screen w-screen bg-[#121212]'>
+    <div className='h-screen w-screen bg-[#121212] relative'>
+     
+      {/* <ProgressBar /> */}
+        <Navbar />
 
-<div className='absolute z-5'>
-      <Navbar />
-
-      <Homepage/>
-</div>
-      <div className='absolute top-0 right-0 bg-transparent z-0'>
-      {/* <Blob/> */}
-      </div>
+        <Homepage />
+ 
+      {/* <div className='absolute top-0 right-0 bg-transparent z-0'> */}
+        {/* <Blob/> */}
+      {/* </div> */}
     </div>
   );
 }
 
-export default App
+export default App;
